48987a3463675020e920129252122275a05c1625
Yashasmadana <85828528+Yashasmadana@users.noreply.github.com>
2023-11-11 23:48:36 +0530

    Update lowest_common_ancestor_BT.cpp

--- lowest_common_ancestor_BT.cpp
+++ lowest_common_ancestor_BT.cpp
@@ -2,14 +2,14 @@
     {
        //Your code here
        
-       if(root==NULL) return NULL;
-       if(root->data==n1 || root->data==n2) return root;
-       
+       if(root==NULL) return NULL; // if the root is not found
+       if(root->data==n1 || root->data==n2) return root; // if both are present in the same subtree
+       // pre-order traversing NLR
        Node* left=lca(root->left,n1,n2);
        Node* right=lca(root->right,n1,n2);
        
        if(left==NULL) return right;
        else if(right==NULL) return left;
        
-       return root;
+       return root; // if both are present in different subtrees
     }
